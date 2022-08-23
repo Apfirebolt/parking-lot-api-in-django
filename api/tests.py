df09e4631d37db2646d843a2ce1cbb7d9d84aea0
Amit P <aspper20@gmail.com>
2022-08-23 21:16:53 +0530

    feat: added sample test cases

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,71 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from . models import CustomUser, Parking, Ticket, Area
+
+from . serializers import CustomUserSerializer
+
+
+AREA_URL = reverse('api:area-create-list')
+
+CREATE_USER_URL = reverse('api:signup')
+TOKEN_URL = reverse('api:signin')
+
+
+def create_user(**params):
+    return CustomUser.objects.create(**params)
+
+
+class PublicAreaApiTests(TestCase):
+    """Test only authenticated users can access to Area URL"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required to access the endpoint"""
+        res = self.client.get(AREA_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'email': 'test@amitpr.com',
+            'password': 'testpass',
+            'username': 'Testusername'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Test creating a user that already exists fails"""
+        payload = {
+            'email': 'test@amitpr.com',
+            'password': 'testpass',
+            'username': 'Testusername',
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+
