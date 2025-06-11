a9dd7faf5015688b1f043b22b2987bc4826b2a88
Apfirebolt <aspper20@gmail.com>
2025-06-11 23:28:29 +0530

    added new models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from uuid import uuid4
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from parking.settings import AUTH_USER_MODEL
 
@@ -12,6 +13,12 @@ SIZE_CHOICES = (
     ("TWO", "Two"),
     ("FOUR-SMALL", "Four-Small"),
     ("FOUR-LARGE", "Four-Large"),
+)
+
+PARKING_TYPE_CHOICES = (
+    ("HOURLY", "Hourly"),
+    ("DAILY", "Daily"),
+    ("MONTHLY", "Monthly"),
 )
 
 
@@ -66,8 +73,10 @@ class Area(models.Model):
 
 class Parking(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='parking_created')
+    name = models.CharField('Parking Name', max_length=100, null=True, blank=True)
+    location = models.CharField('Parking Location', max_length=255, null=True, blank=True)
+    description = models.TextField('Parking Description', null=True, blank=True)
     size = models.CharField('Parking Size', max_length=150, choices=SIZE_CHOICES, default="Four-Small")
-    status = models.CharField('Status', max_length=150, choices=STATUS_CHOICES, default="Empty")
     area = models.ForeignKey(Area, on_delete=models.CASCADE, related_name='area_parkings')
 
     def __str__(self):
@@ -77,12 +86,84 @@ class Parking(models.Model):
         verbose_name_plural = "Parking"
 
 
+class ParkingSection(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    parking = models.ForeignKey(Parking, on_delete=models.CASCADE, related_name='parking_section')
+    floor = models.IntegerField('Floor Number', default=0)
+    parking_type = models.CharField('Parking Type', max_length=50, choices=SIZE_CHOICES, default="FOUR-SMALL")
+    name = models.CharField('Section Name', max_length=100, null=True, blank=True)
+    capacity = models.IntegerField('Capacity', default=0)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Parking Section"
+
+
+class ParkingPrice(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    type = models.CharField('Parking Type', max_length=50, choices=PARKING_TYPE_CHOICES, default="HOURLY")
+    parking_section = models.ForeignKey(ParkingSection, on_delete=models.CASCADE, related_name='parking_price')
+    price = models.FloatField('Parking Price', default=0)
+    vehicle_size = models.CharField('Vehicle Size', max_length=50, choices=SIZE_CHOICES, default="FOUR-SMALL")
+    has_charging = models.BooleanField('Has Charging', default=False)
+
+
+class ParkingSlot(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    section = models.ForeignKey(ParkingSection, on_delete=models.CASCADE, related_name='parking_slot')
+    slot_number = models.CharField('Slot Number', max_length=50, null=True, blank=True)
+    type = models.CharField('Slot Type', max_length=50, choices=SIZE_CHOICES, default="FOUR-SMALL")
+    is_charging_available = models.BooleanField('Is Charging Available', default=False)
+    is_booked = models.BooleanField('Is Booked', default=False)
+    is_reserved = models.BooleanField('Is Reserved', default=False)
+    is_available = models.BooleanField('Is Available', default=True)
+
+    def __str__(self):
+        return self.slot_number
+
+    class Meta:
+        verbose_name_plural = "Parking Slot"
+
+
+class Vehicle(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='parking_user_vehicle')
+    vehicle_number = models.CharField('Vehicle Number', max_length=50, unique=True)
+    vehicle_type = models.CharField('Vehicle Type', max_length=50, choices=SIZE_CHOICES, default="FOUR-SMALL")
+    is_electric = models.BooleanField('Is Electric', default=False)
+    is_active = models.BooleanField('Is Active', default=True)
+
+    def __str__(self):
+        return self.vehicle_number
+
+    class Meta:
+        verbose_name_plural = "Vehicle"
+
+
+class Passes(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='parking_user_passes')
+    parking = models.ForeignKey(Parking, on_delete=models.CASCADE, related_name='parking_passes')
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='vehicle_passes')
+    start_date = models.DateField('Start Date', null=True, blank=True)
+    end_date = models.DateField('End Date', null=True, blank=True)
+    price = models.FloatField('Pass Price', default=0)
+
+    def __str__(self):
+        return str(self.start_date) + ' - ' + str(self.end_date)
+
+    class Meta:
+        verbose_name_plural = "Passes"
+
+
 class Ticket(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='parking_user_tickets')
-    parking = models.ForeignKey(Parking, on_delete=models.CASCADE, related_name='parking_ticket')
+    parking_slot = models.ForeignKey(ParkingSlot, on_delete=models.CASCADE, related_name='parking_slot_ticket', null=True, blank=True)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='vehicle_ticket', null=True, blank=True)
     entry_time = models.DateTimeField('Entry Time', auto_now_add=True)
     exit_time = models.DateTimeField('Exit Time', auto_now=True)
-    price = models.FloatField('Parking Price', default=0)
+    price = models.ForeignKey(ParkingPrice, on_delete=models.CASCADE, related_name='parking_ticket_price')
 
     def __str__(self):
         return str(self.entry_time) + ' - ' + str(self.exit_time)
